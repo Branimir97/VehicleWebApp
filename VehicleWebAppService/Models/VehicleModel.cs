6cd1cd1b24d91ae3fde143cbf01a5af4337fea04
Branimir Butković <branimirb51@gmail.com>
2021-12-28 18:21:39 +0100

    Created VehicleModel model

---
+++ VehicleWebAppService/Models/VehicleModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleWebAppService.Models
+{
+    public class VehicleModel
+    {
+        public int Id { get; set; }
+        public VehicleMake MakeId { get; set; }
+        public string Name { get; set; }
+        public string Abrv { get; set; }
+    }
+}

a87653393eef4cabe137bc90f60d062bb0d344a6
Branimir Butković <branimirb51@gmail.com>
2021-12-28 18:28:55 +0100

    Written some code inside VehicleContext

--- VehicleWebAppService/DAL/VehicleContext.cs
+++ VehicleWebAppService/DAL/VehicleContext.cs
@@ -4,10 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VehicleWebAppService.Models;
 
 namespace VehicleWebAppService.DAL
 {
     public class VehicleContext : DbContext
     {
+        public VehicleContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<VehicleMake> VehicleMakes { get; set; }
+        public DbSet<VehicleModel> VehicleModels { get; set; }
     }
 }

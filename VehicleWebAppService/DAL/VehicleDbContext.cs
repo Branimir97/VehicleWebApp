075b8039070f975cd9723a414abf06960fcccf16
Branimir Butković <branimirb51@gmail.com>
2021-12-28 20:45:54 +0100

    Created Vehicle Initializer

--- VehicleWebAppService/DAL/VehicleDbContext.cs
+++ VehicleWebAppService/DAL/VehicleDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;

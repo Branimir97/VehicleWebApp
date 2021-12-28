3c1ac6e9805330102fce6e6041724224c899f7e6
Branimir Butković <branimirb51@gmail.com>
2021-12-29 00:21:49 +0100

    Removed VehicleInitializer class

--- VehicleWebAppService/DAL/VehicleDbContext.cs
+++ VehicleWebAppService/DAL/VehicleDbContext.cs
@@ -17,13 +17,23 @@ namespace VehicleWebAppService.DAL
         public DbSet<VehicleMake> VehicleMakes { get; set; }
         public DbSet<VehicleModel> VehicleModels { get; set; }
 
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    IConfigurationRoot configuration = new ConfigurationBuilder()
-        //        .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
-        //        .AddJsonFile("appsettings.json")
-        //        .Build();
-        //    optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
-        //}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VehicleMake>().HasData(
+                new VehicleMake { Name = "BMW", Abrv = "BMW" },
+                new VehicleMake { Name = "Ford", Abrv = "Ford" },
+                new VehicleMake { Name = "Audi", Abrv = "Audi" },
+                new VehicleMake { Name = "Porsche", Abrv = "Porsche" },
+                new VehicleMake { Name = "Fiat", Abrv = "Fiat" }
+            );
+
+            modelBuilder.Entity<VehicleModel>().HasData(
+                new VehicleModel { Name = "Focus", MakeId = , Abrv = "Focus" },
+                new VehicleModel { Name = "530xd", MakeId = , Abrv = "F10" },
+                new VehicleModel { Name = "Cayenne 3.0", MakeId = , Abrv = "Cayenne" },
+                new VehicleModel { Name = "Grande Punto", MakeId = , Abrv = "Punto" },
+                new VehicleModel { Name = "A4 2.0TDI", MakeId = , Abrv = "B8.5" }
+            );
+        }
     }
 }

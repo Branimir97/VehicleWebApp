b9d0fd11302fcfccc09cb35c2c020e7179c6ac35
Branimir Butković <branimirb51@gmail.com>
2021-12-29 13:59:11 +0100

    Generated Create view and controller

---
+++ VehicleWebApp/Controllers/CreateController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VehicleWebAppService.DAL;
+
+namespace VehicleWebApp.Controllers
+{
+    public class CreateController : Controller
+    {
+        private readonly VehicleDbContext VehicleDbContext;
+
+        public CreateController(VehicleDbContext vehicleDbContext)
+        {
+            VehicleDbContext = vehicleDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+     }
+}

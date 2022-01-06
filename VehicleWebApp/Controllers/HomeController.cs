0b2424300d2b20c7567cfe414ec929bef2561659
Branimir Butković <branimirb51@gmail.com>
2022-01-06 16:30:06 +0100

    Deleted unused libraries from import

--- VehicleWebApp/Controllers/HomeController.cs
+++ VehicleWebApp/Controllers/HomeController.cs
@@ -1,12 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
-using VehicleWebApp.Models;
 using VehicleWebAppService.DAL;
 
 namespace VehicleWebApp.Controllers

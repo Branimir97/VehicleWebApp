56d7fb73327350722695c1ad4ea404e22a67086c
Branimir Butković <branimirb51@gmail.com>
2022-01-25 10:33:17 +0100

    Fixed VehicleModel Create method using ViewModel

--- VehicleWebApp/ViewModels/VehicleModelDetailsViewModel.cs
+++ VehicleWebApp/ViewModels/VehicleModelDetailsViewModel.cs
@@ -5,5 +5,6 @@ namespace VehicleWebApp.ViewModels
     public class VehicleModelDetailsViewModel
     {
         public VehicleModel VehicleModel { get; set; }
+        public VehicleMake VehicleMake { get; set; }
     }
 }

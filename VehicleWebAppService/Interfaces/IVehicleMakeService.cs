07531a958ac459fc6f598ff336487713a1bdf19e
Branimir Butković <branimirb51@gmail.com>
2022-01-25 13:54:57 +0100

    Displayed pageNumber in VehicleModel index.cshtml

--- VehicleWebAppService/Interfaces/IVehicleMakeService.cs
+++ VehicleWebAppService/Interfaces/IVehicleMakeService.cs
@@ -6,7 +6,8 @@ namespace VehicleWebAppService.Interfaces
 {
     interface IVehicleMakeService
     {
-        public Task<IPagedList<VehicleMake>> GetVehicleMakesBy(string sortOrder, string filter, int? pageNumber);
+        public Task<IPagedList<VehicleMake>> GetVehicleMakesBy(
+            string sortOrder, string filter, int? pageNumber);
         public Task<VehicleMake> GetVehicleMake(int? id);
         public Task AddVehicleMake(VehicleMake vehicleMake);
         public Task UpdateVehicleMake();

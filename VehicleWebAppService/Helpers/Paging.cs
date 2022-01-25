95725d7676cd1ac389eddcc341617bf9684c63b1
Branimir Butković <branimirb51@gmail.com>
2022-01-25 13:47:49 +0100

    Written new paging method for vehicle models

--- VehicleWebAppService/Helpers/Paging.cs
+++ VehicleWebAppService/Helpers/Paging.cs
@@ -17,11 +17,20 @@ namespace VehicleWebAppService.Helpers
             Filtering = new Filtering(dbContext);
         }
 
-        public async Task<IPagedList<VehicleMake>> GetVehicleMakesBy(string sortOrder, string filter, int? pageNumber)
+        public async Task<IPagedList<VehicleMake>> GetVehicleMakesBy(
+            string sortOrder, string filter, int? pageNumber)
         {
             var filteredVehicleMakes = Filtering.FilterVehicleMakes(filter);
             var sortedVehicleMakes = Sorting.SortVehicleMakes(sortOrder, filteredVehicleMakes);
             return await sortedVehicleMakes.ToPagedListAsync(pageNumber ?? 1, 10);
+        }
+
+        public async Task<IPagedList<VehicleModel>> GetVehicleModelsBy(
+            string sortOrder, string filter, int? pageNumber)
+        {
+            var filteredVehicleModels = Filtering.FilterVehicleModels(filter);
+            var sortedVehicleModels = Sorting.SortVehicleModels(sortOrder, filteredVehicleModels);
+            return await sortedVehicleModels.ToPagedListAsync(pageNumber ?? 1, 10);
         }
     }
 }
--- VehicleWebAppService/Interfaces/IPaging.cs
+++ VehicleWebAppService/Interfaces/IPaging.cs
@@ -6,6 +6,9 @@ namespace VehicleWebAppService.Interfaces
 {
     interface IPaging
     {
-        public Task<IPagedList<VehicleMake>> GetVehicleMakesBy(string sortOrder, string filter, int? pageNumber);
+        public Task<IPagedList<VehicleMake>> GetVehicleMakesBy(
+            string sortOrder, string filter, int? pageNumber);
+        public Task<IPagedList<VehicleModel>> GetVehicleModelsBy(
+            string sortOrder, string filter, int? pageNumber);
     }
 }
